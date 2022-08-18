deaec63059cb4f18ca40f2b37d92c725191a53bc
rusta <rustam.ru26@mail.ru>
2022-08-18 20:18:19 +0300

    Commit init

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.test;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import sun.jvm.hotspot.utilities.Assert;
 
 @SpringBootTest
 class TestApplicationTests {
